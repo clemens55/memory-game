2ffc1c9b2c3a8674a9dd04471d97e25b4d771f85
Clemens Krametter <clemi.krammi@gmail.com>
2018-09-10 12:13:36 +0200

    Create a dynamic card list

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 /*
  * Create a list that holds all of your cards
  */
+var cards = ['fa-diamond', 'fa-diamond',
+             'fa-paper-plane-o', 'fa-paper-plane-o',
+             'fa-anchor', 'fa-anchor',
+             'fa-bolt', 'fa-bolt',
+             'fa-cube', 'fa-cube',
+             'fa-leaf', 'fa-leaf',
+             'fa-bicycle', 'fa-bicycle',
+             'fa-bomb', 'fa-bomb'
+            ];
 
+function generateCard(card) {
+    return `<li class="card"><i class="fa ${card}"></i></li>`;
+};
 
 /*
  * Display the cards on the page
@@ -36,6 +48,17 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function initGame() {
+    var deck = document.querySelector(".deck");
+    var cardHTML = cards.map(function(card){
+        return generateCard(card);
+    });
+    
+    deck.innerHTML = cardHTML.join('');
+}
+
+initGame();
 
 var allCards = document.querySelectorAll(".card");
 

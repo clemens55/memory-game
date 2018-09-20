159e6b01f92fc2d3080ef47dccabb4b1bcd5aa1c
Clemens Krametter <clemi.krammi@gmail.com>
2018-09-20 14:49:33 +0200

    Game works, nice

--- js/app.js
+++ js/app.js
@@ -31,17 +31,7 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
+//
 
 function initGame() {
     var deck = document.querySelector('.deck');
@@ -155,7 +145,7 @@ function stopClock() {
 }
 
 //reload
-function myFunction() {
+function reload() {
     location.reload();
 };  
 
@@ -164,9 +154,8 @@ function endGame() {
     const stars = document.getElementsByClassName('fa fa-star');
     var txt;
     if (confirm(`Congrats! You just won the game in ${sec} seconds with ${stars.length}/3 star rating. Do you want to play again?`)) {
-        txt = "You pressed OK!";
+        reload();
     } else {
         txt = "You pressed Cancel!";
     }
-    //document.getElementById("demo").innerHTML = txt;
 }
